3057f822500a2da704fee67834ed3deffca4b8f4
unknown <220477019@mycput.ac.za>
2022-02-04 17:02:33 +0000

    some minor additions

--- src/test/java/za/ac/cput/trevor/TrevorAppTest.java
+++ src/test/java/za/ac/cput/trevor/TrevorAppTest.java
@@ -2,6 +2,10 @@ package za.ac.cput.trevor;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class TrevorAppTest {
+class TrevorAppTest
+{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 
 }
