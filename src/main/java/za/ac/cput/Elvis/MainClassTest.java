8c0c8fb8a6576d47f3099cad46b01c8625e22b8c
Elvis <213063964@mycput.ac.za>
2022-02-06 21:59:24 +0200

    Elvis App is done

---
+++ src/main/java/za/ac/cput/Elvis/MainClassTest.java
@@ -0,0 +1,49 @@
+package za.ac.cput;
+
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainClassTest {
+    private MainClassTest test;
+
+    public MainClassTest() {
+    }
+
+    @BeforeAll
+    public static void setUpClass() {
+    }
+
+    @AfterAll
+    public static void tearDownClass() {
+    }
+
+    @BeforeEach
+    public void setUp() {
+        this.test = new MainClassTest();
+    }
+
+    @AfterEach
+    public void tearDown() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+        // TODO review the generated test code and remove the default call to fail.
+        fail("Timeout.");
+    }
+
+    @Test
+
+    public void testAddition() {
+        int result = test.testingWithTwoNumbers(20, 20);
+        assertEquals(40, result);
+
+    }
+
+    private int testingWithTwoNumbers(int i, int i1) {
+
+        return i;
+    }
+}
